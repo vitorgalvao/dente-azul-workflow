e2717f21342d426aee4945a754134468f74a2c7b
Vítor Galvão <info@vitorgalvao.com>
2025-08-24 22:17:38 +0100

    Remove permission checker

--- Resources/has-bluetooth-permission.swift
+++
@@ -1,8 +0,0 @@
-import CoreBluetooth
-
-if #available(macOS 10.15, *) {
-  guard CBManager.authorization == .allowedAlways else { exit(EXIT_FAILURE) }
-  exit(EXIT_SUCCESS)
-} else {
-  fatalError("Requires macOS 10.15 or later")
-}
